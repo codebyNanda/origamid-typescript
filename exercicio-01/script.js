902c503be9b1c47a09de24197569d07af79cae4e
Ananda Ferreira <ananda.ferreira@raizen.com>
2024-04-17 18:23:44 -0300

    feat: Annotation and Inference exercise

---
+++ exercicio-01/script.js
@@ -0,0 +1,6 @@
+"use strict";
+// Consertar a função utilizando TypeScript
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
+}
+console.log(normalizarTexto(' DEsigN '));
---
+++ exercicio-01/script.ts
@@ -0,0 +1,7 @@
+// Consertar a função utilizando TypeScript
+
+function normalizarTexto(texto: string) {
+  return texto.trim().toLowerCase();
+}
+
+console.log(normalizarTexto(' DEsigN '))
